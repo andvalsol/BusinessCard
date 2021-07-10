ed4ce1e7efa7899c59a6a67f973030a3f26164a6
Andrey Solera <andrey.valverde.solera@gmail.com>
2021-07-10 08:23:54 -0600

    Initial Commit

---
+++ BusinessCars/BusinessCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCarsApp.swift
+//  BusinessCars
+//
+//  Created by Andrey Solera on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
